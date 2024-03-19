7e87686403cc48ab0ecd9bdcbe92046c8679d478
Milton Volpato <volpato.milton@principal.com>
2024-03-19 15:13:23 -0300

    20240319 Commit

--- pipeline_dados/data_raw/README.md
+++ pipeline_dados/data_raw/README.md
@@ -1,1 +1,6 @@
-# Curso de Pandas I/O
+#Crear ambiente python: python3 -m venv venv
+Ativar ambiente: source venv/bin/activate
+Desactivar: deactivate 
+
+Verificar los pacotes instalados:  pip freeze
+pip freeze > requirements.txt
--- pipeline_dados/scripts/Dashboard_Streamlit.py
+++ pipeline_dados/scripts/Dashboard_Streamlit.py
@@ -17,9 +17,31 @@ st.title('DASHBOARD DE VENDAS :shopping_trolley:')
 ##start streamlit run pipeline_dados/scripts/Dashboard_Streamlit.py
 
 url = 'https://labdados.com/produtos'
-response = requests.get(url)
+regioes = ['Brasil', 'Centro-Oeste', 'Nordeste', 'Norte', 'Sudeste', 'Sul']
+
+st.sidebar.title('Filtros')
+regiao = st.sidebar.selectbox('Regiao', regioes)
+
+if regiao == 'Brasil':
+    regiao  = ''
+
+todos_anos = st.sidebar.checkbox('Dados de todo o periodo', value = True)
+                                 
+if todos_anos:
+    ano = ''
+else:
+    ano = st.sidebar.slider('Ano', 2020, 2023)
+
+query_string = {'regiao':regiao.lower(), 'ano': ano}                              
+
+response = requests.get(url, params= query_string)
 dados = pd.DataFrame.from_dict(response.json())
 dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format = '%d/%m/%Y')
+
+###Filtro de Vendedores
+filtro_vendedores = st.sidebar.multiselect('Vendedores', dados['Vendedor'].unique())
+if filtro_vendedores:
+    dados = dados[dados['Vendedor'].isin(filtro_vendedores)]
 
 ## Tabelas
 
